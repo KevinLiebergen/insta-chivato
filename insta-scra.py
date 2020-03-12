11f01f3eb6035a4050c2d9f62f501f440533daa9
kevin <kevinliebergen@hotmail.com>
2020-03-12 22:03:43 +0100

    Connected

--- insta-scra.py
+++ insta-scra.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 import requests
+import argparse
+import json
 
 class InstagramScraper(object):
     def __init__(self, **kwargs):
 
-        default_attr = dict(username='', usernames=[], filename=None,
-                login_user=None, login_pass=None,
-                followings_input=False, followings_output='profiles.txt',
-                destination='./', logger=None, retain_username=False, interactive=False,
-                quiet=False, maximum=0, media_metadata=False, profile_metadata=False, latest=False,
-                latest_stamps=False, cookiejar=None, filter_location=None, filter_locations=None,
-                media_types=['image', 'video', 'story-image', 'story-video'],
-                tag=False, location=False, search_location=False, comments=False,
-                verbose=0, include_location=False, filter=None, proxies={}, no_check_certificate=False,
-                                                                        template='{urlname}', log_destination='')
+        default_attr = dict(username=None, usernames=[], filename=None,
+                login_user=None, login_pass=None, logger=None,
+                followings_input=False, followings_output='profiles.txt')
+
+        allowed_attr = list(default_attr.keys())
+        default_attr.update(kwargs)
+
+        for key in default_attr:
+            if key in allowed_attr:
+                self.__dict__[key] = default_attr.get(key)
+
         self.session = requests.Session()
 
     def authenticate_with_login(self):
@@ -36,30 +39,37 @@ class InstagramScraper(object):
             self.rhx_gis = ""
             print("Connected")
         else:
-            self.logger.error('Login failed for ' + self.login_user)
+            print('Login failed for ' + self.login_user)
 
-            if 'checkpoint_url' in login_text:
-                checkpoint_url = login_text.get('checkpoint_url')
-                self.logger.error('Please verify your account at ' + BASE_URL[0:-1] + checkpoint_url)
-
-                if self.interactive is True:
-                    self.login_challenge(checkpoint_url)
-            elif 'errors' in login_text:
-                for count, error in enumerate(login_text['errors'].get('error')):
-                    count += 1
-                    self.logger.debug('Session error %(count)s: "%(error)s"' % locals())
-            else:
-                self.logger.error(json.dumps(login_text))
-
+            
 def main():	
-    scraper = InstagramScraper()
-    scraper.authenticate_with_login()
 
+    parser = argparse.ArgumentParser(description='Especifica opciones para el script', fromfile_prefix_chars='@')
+    '''
+        You can hide your credentials from the history, by reading your
+        username from a local file:
+        $ instagram-scraper @insta_args.txt user_to_scrape
+        with insta_args.txt looking like this:
+        -u=my_username
+        -p=my_password
+        You can add all arguments you want to that file, just remember to have
+        one argument per line.
+    '''
+    parser.add_argument('--login-user', '--login_user', '-u', default=None, help='Instagram login user')
+    parser.add_argument('--login-pass', '--login_pass', '-p', default=None, help='Instagram login password')
 
+    args = parser.parse_args()
+    
+    scraper = InstagramScraper(**vars(args))
 
+    if args.login_user and args.login_pass:
+        scraper.authenticate_with_login()
+    else:
+        print("Pon usuario y contraseña")
 
 if __name__ == '__main__':
     BASE_URL = 'https://www.instagram.com/'
     LOGIN_URL = BASE_URL + 'accounts/login/ajax/'
     STORIES_UA = 'Instagram 123.0.0.21.114 (iPhone; CPU iPhone OS 11_4 like Mac OS X; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/605.1.15'
+    CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
     main()
